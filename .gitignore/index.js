4257ca6780d5f0779baf8fdbc6b86d51c3ee55b5
razer02 <39990201+razer02@users.noreply.github.com>
2018-06-06 14:04:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,21 +8,20 @@ bot.on('ready', function() {
     console.log("Connected");
 });
 
-bot.login("NDUzODI1MjI5Nzk4NTA2NDk2.DfkldA.0nT_CCA-apB7vG1EBXFqdbbeI6A");
+bot.login("NDUzODI1MjI5Nzk4NTA2NDk2.DfkwhQ.TcNPI6-AkJ0HpvcGa3RAkjLjYCc");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
+        message.channel.sendMessage("Liste des commandes: \n - *help")
     }
 
     if (message.content === "Salut"){
         message.reply("Bien le bonjour :)");
-        console.log("Commande ça va effectué");
+        console.log("Commande Salut effectué);
     }
 
-     if (message.content === "ça va ?"){
-        message.reply("ça va très bien et toi ?");
-        console.log("Commande ça va effectué");
+    if (message.content === "ça va ?"){
+        message.reply("Très bien et toi ?");
+        console.log("Commande Etat effectué);
     }
-
 });
