ba26df872ccae5e6fdb2d6b2933e859b562527f7
razer02 <39990201+razer02@users.noreply.github.com>
2018-06-06 15:32:50 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,23 +11,6 @@ bot.on('ready', function() {
 bot.login(process.env.TOEKN)
 
 bot.on('message', message => {
-
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "général").send(`Bienvenue`)
-})
-
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "général").send(`${member} vient de partir`)
-})
-
-bot.on('guildMemberAdd', member => {
-    var role = member.guild.roles.find('name', 'Immigré');
-    member.addRole(role)
-})
-
-;
-
-bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes: \n - *help")
     }
