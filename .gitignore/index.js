3786f9b434127763e590a9ea2475f4ea290d0819
razer02 <39990201+razer02@users.noreply.github.com>
2018-06-06 16:54:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,16 +10,6 @@ bot.on('ready', function() {
 
 bot.login("NDUzODI1MjI5Nzk4NTA2NDk2.DfkwhQ.TcNPI6-AkJ0HpvcGa3RAkjLjYCc");
 
-bot.on("guildMemberAdd", member => {
-    let role = member.guild.roles.find("name", "ADMIN");
-    member.guild.channels.find("name", "test").send(`Bienvenue ${member.user.username} sur Extasia`)
-    member.addRole("name", "ADMIN")
-})
-
-bot.on("guildMemberRemove"), member => {
-    member.guild.channels.find("name", "test").send(`${member.user.username} vient de partir du serveur`)
-})
-
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("Liste des commandes: \n - *help")
