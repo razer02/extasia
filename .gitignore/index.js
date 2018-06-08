1a621ed9e5d89638a2cc90bedd6e23524b4c2608
razer02 <39990201+razer02@users.noreply.github.com>
2018-06-08 14:52:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -79,6 +79,7 @@ bot.on('message', message => {
 
 bot.on('message', message => {
 
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Vous n'avez pas la permission.");
     if(message.content === prefix + "rappel") {
         message.delete()
         var embed = new Discord.RichEmbed()
