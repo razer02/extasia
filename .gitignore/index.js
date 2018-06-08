bfbf63eeed976184fe7a4c88ac48ec6c702c195d
razer02 <39990201+razer02@users.noreply.github.com>
2018-06-08 12:26:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -79,18 +79,12 @@ bot.on('message', message => {
 
     if(message.content === prefix + "rappel") {
         var embed = new Discord.RichEmbed()
+        message.delete().catch();
         .setDescription("**__Rappel : OBLIGATOIRE__** @everyone")
         .addField("Ces informations sont indispensables pour votre immigration", "Merci de mettre votre **__NOM__** et **__PRÉNOM RP__** sur **__DISCORD__** ainsi que sur **__STEAM__**")
         .setColor("FF0000")
     message.channel.sendEmbed(embed)
         
-    if(message.content === prefix + "say") {
-        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-        // To get the "message" itself we join the `args` back into a string with spaces: 
-        const sayMessage = args.join(" ");
-        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete().catch(O_o=>{}); 
-        // And we get the bot to say the thing: 
-        message.channel.send(sayMessage);
-  }
 }})
+
+
